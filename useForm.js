2dcbadd11b7f97263d146784c9f16c1c4277af6e
sergio.v.p <sergio.velez.padua@gmail.com>
2022-10-21 20:26:29 -0500

    Custom Hooks personalizados

---
+++ useForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+// import { useForm } from '../../src/hooks/useForm';
+
+
+export const useForm = ( initialForm = {} ) => {
+
+    const [ formState, setFormState ] = useState( initialForm );
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+        })
+    }
+
+    const onResetForm = () => {
+        setFormState( initialForm );
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
+    }
+
+
+}
